00cb19a5d69c6a4b24874f152534897cd55630f9
eramsay20 <eramsay10@gmail.com>
2021-07-07 07:16:17 -0400

    Added additional logic to messages API route in order to limit user post capabilities when they are not user1 or user2 in a given conversation. Note that the ability to send messages, as well as the order in which they are displayed when a new ActiveChat window is selected, were fixed in an earlier phase of the challenge.

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -1,43 +1,58 @@
 const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
+const { Op } = require("sequelize");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
   try {
-    if (!req.user) {
-      return res.sendStatus(401);
-    }
+    if (!req.user) return res.sendStatus(401);
+
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
 
-    // if we already know conversation id, we can save time and just add it to message and return
-    if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
+    const conversationExists = await Conversation.findConversation(senderId, recipientId);
+
+    const userAllowedConversations = await Conversation.findAll({
+      where: {
+        [Op.or]: {
+          user1Id: senderId,
+          user2Id: senderId,
+        },
+      },
+    })
+    
+    const allowedConversationExists = userAllowedConversations.find(conversation => conversation.id === conversationId)
+
+    // ER: if no existing convo between req.user and recipient, make a new convo and save the message
+    if (!conversationExists) {
+      let newConversation = await Conversation.create({ user1Id: senderId, user2Id: recipientId });
+
+      if (onlineUsers.includes(sender.id)) sender.online = true;
+
+      const message = await Message.create({
+        senderId,
+        text,
+        conversationId: newConversation.id,
+      });
+
+      return res.json({ message, sender });
+
+    // ER: if a conversation exists between req.user and recipient, but req.user is not user1 or user2 in the convo for which they're trying to post in, return permissions error
+    } else if (conversationExists && !allowedConversationExists){ 
+
+      return res.sendStatus(401);
+
+    // ER: convo exists, and user has permission to post
+    } else {
+      const message = await Message.create({
+        senderId,
+        text,
+        conversationId: conversationId,
+      });
+
       return res.json({ message, sender });
     }
-    // if we don't have conversation id, find a conversation to make sure it doesn't already exist
-    let conversation = await Conversation.findConversation(
-      senderId,
-      recipientId
-    );
-
-    if (!conversation) {
-      // create conversation
-      conversation = await Conversation.create({
-        user1Id: senderId,
-        user2Id: recipientId,
-      });
-      if (onlineUsers.includes(sender.id)) {
-        sender.online = true;
-      }
-    }
-    const message = await Message.create({
-      senderId,
-      text,
-      conversationId: conversation.id,
-    });
-    res.json({ message, sender });
   } catch (error) {
     next(error);
   }
