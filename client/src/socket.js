4bfe5da9be190154e565cded224def95774c46ac
eramsay20 <eramsay10@gmail.com>
2021-07-09 05:54:09 -0400

    Moved client side socket event handlers outside of the connect handler to avoid new handlers being registered every time the Socket reconnects

--- client/src/socket.js
+++ client/src/socket.js
@@ -7,20 +7,24 @@ import {
 } from "./store/conversations";
 
 const socket = io(window.location.origin);
-
+// add user auth here?
 socket.on("connect", () => {
   console.log("connected to server");
+});
 
-  socket.on("add-online-user", (id) => {
-    store.dispatch(addOnlineUser(id));
-  });
+socket.on("add-online-user", (id) => {
+  store.dispatch(addOnlineUser(id));
+});
 
-  socket.on("remove-offline-user", (id) => {
-    store.dispatch(removeOfflineUser(id));
-  });
-  socket.on("new-message", (data) => {
-    store.dispatch(setNewMessage(data.message, data.sender));
-  });
+socket.on("remove-offline-user", (id) => {
+  store.dispatch(removeOfflineUser(id));
+});
+socket.on("new-message", (data) => {
+  store.dispatch(setNewMessage(data.message, data.sender));
 });
 
 export default socket;
+
+// From Socket.io Documentation:
+// Please note that you shouldn’t register event handlers in the connect handler itself, 
+// as a new handler will be registered every time the Socket reconnects
