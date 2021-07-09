864114217ec3ef5464eed271148c4ef83bde34b3
eramsay20 <eramsay10@gmail.com>
2021-07-09 18:45:04 -0400

    refactored Search from Class to Functional; refactores Sidebar and SidebarContainer to use more modern react-redux hooks instead of mapToProps

--- client/src/components/Sidebar/SidebarContainer.js
+++ client/src/components/Sidebar/SidebarContainer.js
@@ -1,42 +1,31 @@
 import React, { useState } from "react";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Sidebar } from "./index";
 import { searchUsers } from "../../store/utils/thunkCreators";
 import { clearSearchedUsers } from "../../store/conversations";
 
-const SidebarContainer = (props) => {
-  const { searchUsers, clearSearchedUsers } = props;
-
+const SidebarContainer = () => {
+  const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleChange = async (event) => {
     if (event.target.value === "") {
       // clear searched convos from redux store
-      clearSearchedUsers();
+      await dispatch(clearSearchedUsers());
       setSearchTerm("");
       return;
     }
     if (searchTerm.includes(event.target.value)) {
-      // if new value is included in search term, we don't need to make another API call, just need to set the search term value so the conversations can be filtered in the rendering
+      // if new value is included in search term, we don't need to make another API call, 
+      // just need to set the search term value so the conversations can be filtered in the rendering
       setSearchTerm(event.target.value);
       return;
     }
-    await searchUsers(event.target.value);
+    await dispatch(searchUsers(event.target.value));
     setSearchTerm(event.target.value);
   };
 
   return <Sidebar handleChange={handleChange} searchTerm={searchTerm} />;
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    searchUsers: (username) => {
-      dispatch(searchUsers(username));
-    },
-    clearSearchedUsers: () => {
-      dispatch(clearSearchedUsers());
-    }
-  };
-};
-
-export default connect(null, mapDispatchToProps)(SidebarContainer);
+export default SidebarContainer;
